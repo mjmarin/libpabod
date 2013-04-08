7e8d48d7664fd94e07f8ad04504ff0e1081a8bda
yoco <yoco@zillizns.com>
2013-04-09 02:49:22 +0800

    loadSbin with new read_number

--- src/model.cpp
+++ src/model.cpp
@@ -787,25 +787,16 @@ void Model::loadInterval (matvar_t *matVar )
 
 void Model::loadSbin (matvar_t *matVar )
 {
-  char *variable = new char [6];
-  int dim = -1;
-  int *el = NULL;
+  std::string variable = "sbin";
 
-  assert (variable != NULL);
-
-  strcpy (variable, "sbin");
-
-  if (existField (matVar, variable))
+  if (exist_field (matVar, variable))
   {
-    readNumber (matVar, variable, &el, &dim);
+    std::vector<int> el = read_number<int>(matVar, variable);
     setSbin (el[0]);
   }
 
   else
     setSbin (-1);
-
-  delete[] variable;
-  delete[] el;
 }
 
 void Model::loadThresh (matvar_t *matVar )
