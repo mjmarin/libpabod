aca8404e2cbdd1ddd561af30e5b7135d24014253
yoco <yoco@zillizns.com>
2013-04-08 21:29:29 +0800

    Simplify and fix several sign/unsign compare

--- src/nms.cpp
+++ src/nms.cpp
@@ -8,77 +8,55 @@ std::vector<int> nms (CvMat *dets, double overlap)
         return std::move(pick);
     }
 
-    std::vector<int> rows(dets->rows);
-    std::iota(rows.begin(), rows.end(), 0);
+    std::vector<double> x1(dets->rows);
+    std::vector<double> y1(dets->rows);
+    std::vector<double> x2(dets->rows);
+    std::vector<double> y2(dets->rows);
+    std::vector<double> areas(dets->rows);
+    std::vector<double> scores(dets->rows);
 
-    CvMat *x1 = sub_mat (dets, rows, {0});
-    std::vector<double> x1Ptr = get_mat_data<double>(x1);
+    // collect coordinate, area, score
+    for(int i = 0; i < dets->rows; ++i) {
+        x1[i]     = cvGetReal2D(dets, i, 0);
+        y1[i]     = cvGetReal2D(dets, i, 1);
+        x2[i]     = cvGetReal2D(dets, i, 2);
+        y2[i]     = cvGetReal2D(dets, i, 3);
+        areas[i]  = (x2[i] - x1[i] + 1) * (y2[i] - y1[i] + 1);
+        scores[i] = cvGetReal2D(dets, i, 5);
+    }
 
-    CvMat *y1 = sub_mat (dets, rows, {1});
-    std::vector<double> y1Ptr = get_mat_data<double>(y1);
-
-    CvMat *x2 = sub_mat (dets, rows, {2});
-    std::vector<double> x2Ptr = get_mat_data<double>(x2);
-
-    CvMat *y2 = sub_mat (dets, rows, {3});
-    std::vector<double> y2Ptr = get_mat_data<double>(y2);
-
-    CvMat *s = sub_mat (dets, rows, {5});
-    std::vector<double> sPtr = get_mat_data<double>(s);
-
-    int areaDims[2] = {dets->rows, 1};
-
-    CvMat *area;
-    createMatrix (2, areaDims, CV_64FC1, &area);
-    CvMat *p1, *p2;
-    p1 = cvCloneMat (x1);
-    p2 = cvCloneMat (y1);
-    cvSub (x2, x1, p1);
-    cvAddS (p1, cvScalar(1), p1);
-    cvSub (y2, y1, p2);
-    cvAddS (p2, cvScalar(1), p2);
-    cvMul (p1, p2, area);
-
-    std::vector<double> areaPtr = get_mat_data<double>(area);
-
-    cvReleaseMat (&x1);
-    cvReleaseMat (&y1);
-    cvReleaseMat (&x2);
-    cvReleaseMat (&y2);
-    cvReleaseMat (&x1);
-    cvReleaseMat (&s);
-    cvReleaseMat (&area);
-    cvReleaseMat (&p1);
-    cvReleaseMat (&p2);
-
-    std::vector<size_t> idx;
-
-    shell_sort (sPtr, ASCEND, idx);
+    // sort by score, but track index
+    // see http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
+    // and http://stackoverflow.com/questions/10580982/c-sort-keeping-track-of-indices
+    std::vector<size_t> idx(dets->rows);
+    std::iota(idx.begin(), idx.end(), static_cast<size_t>(0));
+    std::sort(idx.begin(), idx.end(), [&scores](size_t i1, size_t i2) {
+        return scores[i1] < scores[i2];
+    });
 
     while (idx.size() > 0)
     {
-        int last = idx.size()-1;
-        int i = idx[last];
+        size_t last = idx.size()-1;
+        size_t i = idx[last];
         pick.push_back(i);
 
-        std::vector<size_t> suppress;
-        suppress.push_back(last);
+        std::vector<size_t> suppress {last};
 
-        for (int pos = 0; pos < last; pos++)
+        for (size_t pos = 0; pos < last; pos++)
         {
             int j = idx[pos];
 
-            int xx1 = max (x1Ptr[i], x1Ptr[j]);
-            int yy1 = max (y1Ptr[i], y1Ptr[j]);
-            int xx2 = min (x2Ptr[i], x2Ptr[j]);
-            int yy2 = min (y2Ptr[i], y2Ptr[j]);
+            int xx1 = max (x1[i], x1[j]);
+            int yy1 = max (y1[i], y1[j]);
+            int xx2 = min (x2[i], x2[j]);
+            int yy2 = min (y2[i], y2[j]);
             int w = xx2 - xx1 + 1;
             int h = yy2 - yy1 + 1;
 
             if (w > 0 && h > 0)
             {
                 // Compute overlap
-                double o = (w * h) / areaPtr[j];
+                double o = (w * h) / areas[j];
 
                 if (o > overlap)
                 {
